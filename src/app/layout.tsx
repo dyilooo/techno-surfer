7c1454ed451457f9e48b9f330cf58c6f58f83ca9
Gelo <martangelomartinez@gmail.com>
2025-03-27 22:38:53 +0800

    TechnoSurfer as the name of the app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'DigiMarkPro ',
+  title: 'TechnoSurfer',
 }
 
 export default function RootLayout({
